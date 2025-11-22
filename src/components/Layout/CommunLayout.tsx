6ef8dcda16d3e808a2c408840ce0234d87c97d1b
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-22 15:49:27 -0300

    refactor: add functions and more components to CommunLayout

--- src/components/Layout/CommunLayout.tsx
+++ src/components/Layout/CommunLayout.tsx
@@ -3,44 +3,55 @@ import type { CommunLayoutProps } from '../../types/data.type'
 import ButtonSelect from '../../utils/ButtonSelect'
 import InformationBlock from '../InformationBlock'
 import ImgSelect from '../../utils/ImgSelect';
-export type Tabs = "overview" | "structure" | "geology";
+import ButtonSelectV2 from '../../utils/ButtonSelectV2';
+import Nav from '../../Nav/Nav'
+export type Tabs = "overview" | "structure" | "geology" ;
 const CommunLayout = ({data}:CommunLayoutProps) => {
-  const [selectTab, setSelectTab] = useState<Tabs>('overview')
+  const [selectTab, setSelectTab] = useState<Tabs>("overview")
   const handleTabClick = (tab:Tabs) => setSelectTab(tab)
+  const [navBar, setNavBar] = useState(false)
+  const handleNavBar = () => setNavBar(!navBar);
 
   return (
-    <main className='text-white'>
-       <div className='flex flex-col xl:flex-row  pt-25 px-50 items-center justify-between content-center'>
+    <div>
+      <Nav navBar={navBar} handleNavBar={handleNavBar} />
+      <div className={`${!navBar ? 'flex flex-col' : 'hidden md:flex'}`}>
+          <div className='md:hidden'><ButtonSelectV2 name={data.name} handleTabClick={handleTabClick}  selectTab={selectTab} /></div>
+        <main className='text-white'>
+       <div className='flex flex-col xl:flex-row  pt-25 xl:pt-15 px-50 xl:px-35 items-center justify-between content-center'>
           <div className='my-4   xl:my-0  '> <ImgSelect selectTab={selectTab} data={data} /></div>
-           <section className='w-100 md:w-125 lg:w-100 text-center lg:text-left px-7 '>
-                <h1 className='font-bold text-[em] uppercase my-5 text-2xl'>{data.name}</h1>
+           <section className='w-100 md:w-125 lg:w-100 xl:w-150 text-center lg:text-left px-7 '>
+                <h1 className='font-bold text-[1.5em] uppercase my-5 text-2xl xl:text-[3.5rem]'>{data.name}</h1>
                 <InformationBlock selectTab={selectTab} data={data} />
-               <div className='flex mt-4 md:flex md:ml-7 pt-2.5 xl:ml-0 md:w-120 '>
-                  <ButtonSelect name={data.name} valor='overview' handleTabClick={handleTabClick} selectTab='01 Overview' />
-                  <ButtonSelect name={data.name} valor='structure' handleTabClick={handleTabClick} selectTab='02 Internal Structure' />
-                  <ButtonSelect name={data.name} valor='geology' handleTabClick={handleTabClick} selectTab='03 Surface Geology' />
+               <div className='flex mt-4 md:flex  pt-2.5  md:w-120 xl:flex-col xl:w-[100%] '>
+                  <ButtonSelect name={data.name} valor="overview" handleTabClick={handleTabClick}  selectTab={selectTab} title='01 Overview' />
+                  <ButtonSelect name={data.name} valor="structure" handleTabClick={handleTabClick} selectTab={selectTab} title='02 Internal Structure' />
+                  <ButtonSelect name={data.name} valor="geology" handleTabClick={handleTabClick}  selectTab={selectTab} title='03 Surface Geology' />
                 </div>
            </section>
        </div>
-       <div className='flex flex-col  xl:flex-row justify-center items-center gap-5 mt-15 p-5'>
-         <article className='w-full  md:w-40 h-25 p-4 border border-gray-400 rounded-[7px]  gap-2.5 items-start justify-center flex-col'>
-           <h2 className='uppercase text-[.7em] mb-2.5'>Rotation time</h2>
-           <h3 className='text-[1.2em] font-bold'>{data.rotation}</h3>
+       <div className='flex flex-col   md:flex-row justify-center items-center gap-5 mt-15 p-5'>
+         <article className='w-full flex  flex-row md:w-40 xl:w-60  h-25 p-4 border border-gray-400 rounded-[7px] 
+          gap-2.5 items-center justify-between flex-col '>
+           <div><h2 className='uppercase text-[1.2em] font-semibold '>Rotation time</h2></div>
+           <div><h3 className='text-[1.2em] font-bold'>{data.rotation}</h3></div>
          </article>
-         <article className='w-full  md:w-40 h-25  p-4  border border-gray-400 rounded-[7px] gap-2.5 items-start justify-center flex-col'>
-            <h2 className='uppercase text-[.7em] mb-2.5'>Revolution time</h2>
-            <h3 className='text-[1.2em] font-bold'>{data.revolution}</h3>
+         <article className='w-full flex flex-row md:w-40  xl:w-60 h-25  p-4  border border-gray-400 rounded-[7px] gap-2.5 items-center justify-between flex-col'>
+            <div><h2 className='uppercase text-[1.2em] font-semibold'>Revolution time</h2></div>
+           <div> <h3 className='text-[1.2em] font-bold'>{data.revolution}</h3></div>
          </article>
-         <article className='w-full  md:w-40 h-25  p-4  border border-gray-400 rounded-[7px] gap-2.5 items-start justify-center flex-col'>
-            <h2 className='uppercase text-[.7em] mb-2.5'>Radius</h2>
+         <article className='w-full flex flex-row md:w-40 xl:w-60  h-25  p-4  border border-gray-400 rounded-[7px] gap-2.5 items-center justify-between flex-col'>
+            <h2 className='uppercase text-[1.2em] font-semibold'>Radius</h2>
              <h3 className='text-[1.2em] font-bold'>{data.radius}</h3>
          </article>
-         <article className='w-full  md:w-40 h-25  p-4  border border-gray-400 rounded-[7px] gap-2.5 items-start justify-center flex-col'>
-             <h2 className='uppercase text-[.7em] mb-2.5'>Average temp.</h2>
-              <h3 className='text-[1.2em] font-bold'>{data.temperature}</h3>
+         <article className='w-full flex flex-row md:w-40 xl:w-60  h-25  p-4  border border-gray-400 rounded-[7px] gap-2.5 items-center justify-between flex-col'>
+            <div> <h2 className='uppercase text-[1.2em] font-semibold'>Average temp.</h2></div>
+             <div> <h3 className='text-[1.2em] font-bold'>{data.temperature}</h3></div>
          </article>
        </div>
     </main>
+      </div>
+    </div>
   )
 }
 
