298bd950a72294088dc2477a4e962b3b4f89880b
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-22 15:46:45 -0300

    refactor: add an Prop called title to ButtonSelect

--- src/utils/ButtonSelect.tsx
+++ src/utils/ButtonSelect.tsx
@@ -1,33 +1,33 @@
 import type { buttonProps } from '../components/Button'
 import Button from '../components/Button'
-const ButtonSelect = ({name,valor,handleTabClick,selectTab}:buttonProps) => {
+const ButtonSelect = ({name,valor,handleTabClick,title,selectTab}:buttonProps) => {
    switch(name) {
-      case  'Mercury':
-        return <Button handleTabClick={handleTabClick}  color='bg-red-400' valor={valor} selectTab={selectTab} />
+      case  'Mercury': 
+        return <Button handleTabClick={handleTabClick}  color='bg-gray' valor={valor} title={title} selectTab={selectTab} />
       break  
       case  'Venus':
-        return <Button handleTabClick={handleTabClick}  color='bg-red-400' valor={valor} selectTab={selectTab} />
+        return <Button handleTabClick={handleTabClick}  color='bg-sunset-orange' valor={valor} title={title} selectTab={selectTab} />
       break  
       case  'Earth':
-        return <Button handleTabClick={handleTabClick}  color='bg-red-400' valor={valor} selectTab={selectTab} />
+        return <Button handleTabClick={handleTabClick}  color='bg-azure' valor={valor} title={title} selectTab={selectTab} />
       break  
       case  'Mars':
-        return <Button handleTabClick={handleTabClick}  color='bg-red-400' valor={valor} selectTab={selectTab} />
+        return <Button handleTabClick={handleTabClick}  color='bg-red' valor={valor} title={title} selectTab={selectTab} />
       break  
       case  'Jupiter':
-        return <Button handleTabClick={handleTabClick}  color='bg-red-400' valor={valor} selectTab={selectTab} />
+        return <Button handleTabClick={handleTabClick}  color='bg-sunset-orange' valor={valor} title={title} selectTab={selectTab} />
       break  
       case  'Saturn':
-        return <Button handleTabClick={handleTabClick}  color='bg-red-400' valor={valor} selectTab={selectTab} />
+        return <Button handleTabClick={handleTabClick}  color='bg-sunset-orange' valor={valor} title={title} selectTab={selectTab} />
       break  
       case  'Uranus':
-        return <Button handleTabClick={handleTabClick}  color='bg-red-400' valor={valor} selectTab={selectTab} />
+        return <Button handleTabClick={handleTabClick}  color='bg-blue-300' valor={valor} title={title} selectTab={selectTab} />
       break  
       case  'Neptune':
-        return <Button handleTabClick={handleTabClick}  color='bg-red-400' valor={valor} selectTab={selectTab} />
+        return <Button handleTabClick={handleTabClick}  color='bg-ocean-blue' valor={valor} title={title} selectTab={selectTab} />
       break  
       default:
-        return <Button handleTabClick={handleTabClick}  color={'bg-gray-300'} valor={valor} selectTab={selectTab} />
+        return <Button handleTabClick={handleTabClick}  color='bg-azure' valor={valor} title={title} selectTab={selectTab} />
       break  
    }
 }
