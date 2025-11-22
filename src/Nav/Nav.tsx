9ebe7ca0a84c5167e570e3ab6732dccd3a5f8b8b
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-22 15:45:38 -0300

    refactor: update style and change onclick on button to <li>

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -1,21 +1,26 @@
-import { useState } from 'react'
 import { NavItem } from '../components/NavItem'
 import {NavList} from '../Nav/data/NavList'
 import Hamburger from '../assets/icon-hamburger.svg'  
-const Nav = () => {
 
-  const [navBar, setNavBar] = useState(false)
+export type NavProps = {
+  navBar: boolean ,
+  handleNavBar: () => void
+}
+
+const Nav = ({navBar, handleNavBar}:NavProps ) => {
 
   return (
-    <nav className='flex flex-col px-2 xl:flex-row md:justify-center md:border-b md:gap-5 md:border-gray-50 
-      md:items-center xl:justify-between text-white xl:h-25 xl:py-10 ' >
+    <nav className='flex flex-col px-2 w-full xl:flex-row md:justify-center
+     border-b md:gap-5 border-gray-50 md:items-center xl:justify-between text-white xl:h-25 xl:py-10 ' >
       <div className='p-2 my-7 md:my-1.5 flex flex-row justify-between items-center'>
-          <a href='#' className="uppercase pl-2.5 font-medium ">The Planets</a>
-          <button className='md:hidden' onClick={() => setNavBar(!navBar)}><img src={Hamburger}/></button>
+          <a href='#' className="uppercase pl-2.5 font-bold ">The Planets</a>
+          <button className='md:hidden' onClick={handleNavBar}><img src={Hamburger}/></button>
        </div>
       
-        <ul className={` ${navBar ? 'flex-col md:flex' : 'hidden md:flex' }
-       md:flex-row xl:mr-4 md:flex-wrap  md:justify-center md:gap-2.5 space-y-4`}>
+        <ul className={` ${navBar ? 'flex-col md:flex' : 'hidden md:flex ' }
+       md:flex-row xl:mr-4 md:flex-wrap px-4 lg:px-0  md:justify-center md:gap-2.5 space-y-4 pt-5 fixed top-25 md:top-[-25px] xl:top-[-5px] xl:w-auto xl:h-auto
+        left-0 w-full h-full md:relative
+         bg-deep-space-blue z-100 h-max-full `}>
           {
              NavList.map(({path,name, color}, index) => (
                 <NavItem key={index} path={path} name={name} color={color} />
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -12,10 +12,10 @@ export const NavItem = ({path, name, color}:NavListProps) => {
  const navigate = useNavigate()
  
   return (
-    <li className="list-none p-2 md:mt-2.5 ">
+    <li className="list-none p-2 md:mt-2.5" onClick={() => navigate(path)} >
         <div className="flex flex-row justify-between items-center px-2">
-          <button onClick={() => navigate(path)} className="bg-trasparent uppercase 
-          flex flex-row items-center gap-2.5 cursor-pointer">
+          <button className="bg-trasparent uppercase 
+          flex flex-row items-center gap-2.5 cursor-pointer font-semibold hover:text-gray-400">
            <DotColor color={color}/>{name}</button>
            <img src={Arrow} alt=''  className='w-2.5 md:hidden' />
         </div>
