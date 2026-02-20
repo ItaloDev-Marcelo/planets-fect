8247db518072b2f12e67f4b44527773ff5d1429f
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-20 13:54:40 -0300

    docs: move to folder with proper name

--- src/components/FigureContainer.tsx
+++
@@ -1,7 +0,0 @@
-import type { FigureContainerProps } from '../types/figure.type'
-
-const FigureContainer = ({children, style}:FigureContainerProps) => {
-  return <figure className={style}>{children}</figure>
-}
-
-export default FigureContainer
