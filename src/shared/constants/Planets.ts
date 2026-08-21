a1f0ecc813800da1589b4a88aaa7b4c128bcaefe
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-21 15:27:39 -0300

    perf: creating planentInfo to organize my data

---
+++ src/shared/constants/Planets.ts
@@ -0,0 +1,13 @@
+import Data from '../../data/data.json';
+
+export const planentInfo = {
+  Mercury: Data[0],
+  Venus: Data[1],
+  Earth: Data[2],
+  Mars: Data[3],
+  Jupiter: Data[4],
+  Saturn: Data[5],
+  Uranus: Data[6],
+  Neptune: Data[7]
+}
+
