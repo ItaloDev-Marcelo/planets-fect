58bb577113dc340d8d1c435e7bda4b3921c643db
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-21 15:28:16 -0300

    perf: organize colors creating File Colors

---
+++ src/shared/constants/Colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+   Mercury:  'gray',
+   Venus:   'sunset-orange',
+   Earth: 'azure',
+   Mars: 'red',
+   Jupiter: 'sunset-orange',
+   Saturn: 'sunset-orange',
+   Uranus: 'blue-300',
+   Neptune: 'ocean-blue'
+} as const
