b0e4d50a7473597867785675c4c574deb22191db
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-21 17:18:31 -0300

    refactor: change importing name from DotColor to Dot also removing useNavigate

--- src/components/Nav/NavItem.tsx
+++ src/components/Nav/NavItem.tsx
@@ -1,20 +1,23 @@
-import { useNavigate } from "react-router-dom"
-import {DotColor} from '../dot/DotColor'
+import {Dot} from '../Dot/Dot'
 import Arrow from '../../assets/icon-chevron.svg'
-import type { NavListProps } from "../../types/nav.list"
+import type { NavListProps } from "../../shared/types/nav.list"
+import { Link } from "@tanstack/react-router"
 
-export const NavItem = ({path, name, color}:NavListProps) => {
- const navigate = useNavigate()
+export const NavItem = ({path, name, color, handleNavBar}:NavListProps) => {
+  
+
 
   return (
-    <li className="list-none p-2 md:mt-2.5" onClick={() => navigate(path)} >
+   <Link to={path} onClick={handleNavBar}>
+       <li className="list-none p-2 md:mt-2.5" >
         <div className="flex flex-row justify-between items-center px-2">
           <button className="bg-trasparent uppercase 
           flex flex-row items-center gap-2.5 cursor-pointer font-semibold hover:text-gray-400">
-           <DotColor color={color}/>{name}</button>
+           <Dot color={color}/>{name}</button>
            <img src={Arrow} alt=''  className='w-2.5 md:hidden' />
         </div>
         <hr className="mt-5 md:hidden text-gray-200"/>
     </li>
+   </Link>
   )
 }
