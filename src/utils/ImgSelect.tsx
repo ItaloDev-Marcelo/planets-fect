f42ace902109374c036f3aea0421f8fc5324b952
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-21 15:30:09 -0300

    style: add more style

--- src/utils/ImgSelect.tsx
+++ src/utils/ImgSelect.tsx
@@ -1,19 +1,19 @@
-import ImageContent from "../components/ImageContainers/ImageContent"
-import FigureContainer from "../components/FigureContainer"
-import type { InforBlockProps } from "../types/data.type"
+import ImageContent from "../components/ImageContainers/Container"
+import FigureContainer from "../components/ImageContainers/FigureContainer"
+import type { InforBlockProps } from "../shared/types/data.type"
 const ImgSelect = ({selectTab, data}:InforBlockProps) => {
    switch(selectTab) {
       case  "overview":
-        return <FigureContainer><ImageContent style='min-w-40 md:max-w-75 '  url={data.images.planet}/></FigureContainer>
+        return <FigureContainer><ImageContent style='min-w-30 px-20 md:p-0 md:max-w-75 xl:max-w-125 '  url={data.images.planet}/></FigureContainer>
       break  
       case  "structure":
-        return <FigureContainer><ImageContent style='min-w-40 md:max-w-75' url={data.images.internal}/></FigureContainer>
+        return <FigureContainer><ImageContent style='min-w-30 px-20 md:p-0 md:max-w-75 xl:max-w-125' url={data.images.internal}/></FigureContainer>
       break  
       case  "geology":
         return <FigureContainer style='relative'>
-             <ImageContent style='min-w-40 md:max-w-75' url={data.images.planet}/>
-             <div className='absolute top-22 left-26 md:top-40 md:left-50 '>
-                  <ImageContent style="min-w-20  md:max-w-40 rotate-320" url={data.images.geology}/>
+             <ImageContent style='min-w-30 px-20 md:p-0 md:max-w-75 xl:max-w-125' url={data.images.planet}/>
+             <div className='absolute top-22 left-48 md:top-40 xl:left-80 xl:top-75 md:left-50 '>
+                  <ImageContent style="w-30  md:max-w-40 xl:w-110 rotate-320" url={data.images.geology}/>
              </div>
         </FigureContainer>
       break  
