8bc5b2e04ee1d6cc8c41f3b5db1e765fea2cec0b
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-23 11:16:50 -0300

    style: add more style

--- src/utils/ImgSelect.tsx
+++ src/utils/ImgSelect.tsx
@@ -4,16 +4,16 @@ import type { InforBlockProps } from "../shared/types/data.type"
 const ImgSelect = ({selectTab, data}:InforBlockProps) => {
    switch(selectTab) {
       case  "overview":
-        return <FigureContainer><ImageContent name={data.name} style='min-w-30 px-20 md:p-0 md:max-w-75 xl:min-w-75 '  url={data.images.planet}/></FigureContainer>
+        return <FigureContainer><ImageContent name={data.name} style='md:w-75 xl:w-100 px-20 md:p-0   '  url={data.images.planet}/></FigureContainer>
       break  
       case  "structure":
-        return <FigureContainer><ImageContent name={data.name} style='min-w-30 px-20 md:p-0 md:max-w-75 xl:min-w-75' url={data.images.internal}/></FigureContainer>
+        return <FigureContainer><ImageContent name={data.name} style='md:w-75 xl:w-100 px-20 md:p-0  ' url={data.images.internal}/></FigureContainer>
       break  
       case  "geology":
         return <FigureContainer style='relative'>
-             <ImageContent name={data.name} style='min-w-30 px-20 md:p-0 md:max-w-75 xl:min-w-75' url={data.images.planet}/>
+             <ImageContent name={data.name} style='md:w-75 xl:w-100 px-20 md:p-0  ' url={data.images.planet}/>
              <div className='absolute top-22 left-48 md:top-40 xl:left-60 xl:top-60 md:left-50 '>
-                  <ImageContent style="w-30  md:max-w-40 xl:w-110 rotate-320" url={data.images.geology}/>
+                  <ImageContent style="w-15 md:w-30   md:max-w-40 xl:w-110 rotate-320" url={data.images.geology}/>
              </div>
         </FigureContainer>
       break  
